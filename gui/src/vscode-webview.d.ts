70190ca337001cf86bce766ddfe6c21edffe30aa
Duke Pan <matthewdukepan@gmail.com>
2024-10-20 15:29:38 -0700

    Remove useless file and logs

--- gui/src/vscode-webview.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "vscode-webview" {
-  export interface WebviewApi<T> {
-    postMessage(message: T): void;
-    getState(): T | undefined;
-    setState<S extends T>(newState: S): S;
-  }
-}
