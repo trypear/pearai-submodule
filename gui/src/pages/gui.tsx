e81103c770edefe7a9d605cb8dadc918d26d6c13
Himanshu <him.speedo@gmail.com>
2024-10-25 00:00:14 +0530

    stop the jumps for now

--- gui/src/pages/gui.tsx
+++ gui/src/pages/gui.tsx
@@ -220,6 +220,10 @@ function GUI() {
     getLocalStorage("showTutorialCard"),
   );
 
+  // console.dir(window.isPearOverlay)
+  // console.dir(defaultModel.title)
+  // console.dir(location.pathname)
+
 
 
   // AIDER HINT BUTTON HIDDEN IN V1.4.0
@@ -292,19 +296,19 @@ function GUI() {
     setIsAtBottom(true);
   }, [active]);
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      window.scrollTo({
-        top: topGuiDivRef.current?.scrollHeight,
-        behavior: "instant" as any,
-      });
-    }, 1);
+  // useEffect(() => {
+  //   const timeoutId = setTimeout(() => {
+  //     window.scrollTo({
+  //       top: topGuiDivRef.current?.scrollHeight,
+  //       behavior: "instant" as any,
+  //     });
+  //   }, 1);
 
-    return () => {
-      clearTimeout(timeoutId)
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [topGuiDivRef.current]);
+  //   return () => {
+  //     clearTimeout(timeoutId)
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [topGuiDivRef.current]);
 
   useEffect(() => {
     // Cmd + Backspace to delete current step
