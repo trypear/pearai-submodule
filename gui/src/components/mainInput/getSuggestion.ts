8c465955ebd23d8b2675f011dc5b11c4c000c235
Himanshu <him.speedo@gmail.com>
2024-10-31 14:48:11 +0530

    remove add more context providers from perplexity

--- gui/src/components/mainInput/getSuggestion.ts
+++ gui/src/components/mainInput/getSuggestion.ts
@@ -5,7 +5,7 @@ import tippy from "tippy.js";
 import { IIdeMessenger } from "../../context/IdeMessenger";
 import MentionList from "./MentionList";
 import { ComboBoxItem, ComboBoxItemType, ComboBoxSubAction } from "./types";
-import { isBareChatMode } from "@/util/bareChatMode";
+import { isBareChatMode, isPerplexityMode } from "@/util/bareChatMode";
 
 function getSuggestion(
   items: (props: { query: string }) => Promise<ComboBoxItem[]>,
@@ -115,6 +115,8 @@ export function getContextProviderDropdownOptions(
 ) {
 
   const bareChatMode = isBareChatMode();
+  const isPerplexity = isPerplexityMode();
+
 
   const items = async ({ query }) => {
     if (inSubmenu.current) {
@@ -165,6 +167,7 @@ export function getContextProviderDropdownOptions(
       });
     } else if (
       !bareChatMode &&
+      !isPerplexity &&
       mainResults.length === availableContextProvidersRef.current.length
     ) {
       mainResults.push({
