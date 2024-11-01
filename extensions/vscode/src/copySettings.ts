36f0910ea93e8c0a1c647b5eea099809154cba2f
nang-dev <contactnang88@gmail.com>
2024-10-31 18:10:34 -0700

    Added exclusions

--- extensions/vscode/src/copySettings.ts
+++ extensions/vscode/src/copySettings.ts
@@ -60,13 +60,16 @@ function copyVSCodeSettingsToPearAIDir() {
     const platform = process.platform;
     const arch = process.arch;
 
+     // Built-in extensions
+    exclusions.push('supermaven');
     if (platform === "darwin" && arch === "arm64") {
-        exclusions.push('vscode-pylance');
+        exclusions.push('ms-python.vscode-pylance');
+        exclusions.push('ms-python.python');
     }
 
     // EXCLUDE CONFLICTING EXTENSIONS
     exclusions.push('codium');
-    exclusions.push('copilot');
+    exclusions.push('github.copilot');
     exclusions.push('continue');
 
     copyDirectoryRecursiveSync(vscodeExtensionsDir, pearAIDevExtensionsDir, exclusions);
