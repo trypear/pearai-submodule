a82bb95f87610fdd00d346e961ba72a2b8715a23
Himanshu <him.speedo@gmail.com>
2024-11-16 16:43:17 +0530

    linting

--- gui/src/pages/onboarding/OnboardingTutorial.tsx
+++ gui/src/pages/onboarding/OnboardingTutorial.tsx
@@ -69,7 +69,7 @@ const TutorialCardDiv = styled.div`
   overflow: hidden;
 `;
 
-const ContentWrapper = styled(motion.div)<{ direction: 'left' | 'right' }>`
+const ContentWrapper = styled(motion.div) <{ direction: 'left' | 'right' }>`
   opacity: 0;
   margin-top: 0.5rem;
   border-top: 1px solid ${vscInputBorderFocus};
@@ -118,24 +118,24 @@ const OnboardingTutorial: React.FC<OnboardingTutorialProps> = ({ onClose, onExam
   const pages = [
     {
       title: (<h3>Select Code and Chat (<kbd>{getMetaKeyAndShortcutLabel()}</kbd>+<kbd>L</kbd>)</h3>),
-      description: <p>Select few lines of code, and press <b><kbd className="font-mono">{getMetaKeyAndShortcutLabel()}</kbd> <kbd className="font-mono">L</kbd></b> to add it to this chat box context.<br/><br/>
-      <span>Don't have a file open?</span>
-      <br/>
-      <span className='mt-2'> Click here <kbd className="no-underline font-mono decoration-current hover:underline cursor-pointer font-bold" onClick={() => ideMessenger.post("showTutorial", undefined)}>pearai_tutorial.py</kbd> to open a sample file</span>
-      {noCodeSelectedMsg && (
-        <div className="bg-yellow-200 text-yellow-800 p-2 rounded mb-2">
-          {noCodeSelectedMsg}
-        </div>
-      )}
+      description: <p>Select few lines of code, and press <b><kbd className="font-mono">{getMetaKeyAndShortcutLabel()}</kbd> <kbd className="font-mono">L</kbd></b> to add it to this chat box context.<br /><br />
+        <span>Don't have a file open?</span>
+        <br />
+        <span className='mt-2'> Click here <kbd className="no-underline font-mono decoration-current hover:underline cursor-pointer font-bold" onClick={() => ideMessenger.post("showTutorial", undefined)}>pearai_tutorial.py</kbd> to open a sample file</span>
+        {noCodeSelectedMsg && (
+          <div className="bg-yellow-200 text-yellow-800 p-2 rounded mb-2">
+            {noCodeSelectedMsg}
+          </div>
+        )}
       </p>,
     },
     {
       description: (
         <>
           <p>Ask a question about the code you just highlighted in the chat below!</p>
-          <DelayedMessage 
-            message="Press the right arrow below for the next step." 
-            delay={10000} 
+          <DelayedMessage
+            message="Press the right arrow below for the next step."
+            delay={10000}
           />
         </>
       ),
@@ -154,14 +154,14 @@ const OnboardingTutorial: React.FC<OnboardingTutorialProps> = ({ onClose, onExam
     },
     {
       title: <h3>Inline Code Editing (<kbd>{getMetaKeyAndShortcutLabel()}</kbd>+<kbd>I</kbd>)</h3>,
-  description: <p>After the changes appear, you can:<ul className="list-disc marker:text-foreground" >
-                                                                  <li><b>accept all changes with <kbd>{getMetaKeyAndShortcutLabel()}+SHIFT+ENTER</kbd></b>,</li>
-                                                                  <li>or <b>reject all changes with <kbd>{getMetaKeyAndShortcutLabel()}+SHIFT+BACKSPACE</kbd></b></li>
-                                                                </ul></p>,
+      description: <p>After the changes appear, you can:<ul className="list-disc marker:text-foreground" >
+        <li><b>accept all changes with <kbd>{getMetaKeyAndShortcutLabel()}+SHIFT+ENTER</kbd></b>,</li>
+        <li>or <b>reject all changes with <kbd>{getMetaKeyAndShortcutLabel()}+SHIFT+BACKSPACE</kbd></b></li>
+      </ul></p>,
     },
     {
       title: <h3>Codebase Context (<kbd>{getMetaKeyAndShortcutLabel()}</kbd>+<kbd>ENTER</kbd>)</h3>,
-      description: <p >Almost done! Try asking anything about your general codebase by prompting then pressing <b><kbd>{getMetaKeyAndShortcutLabel()}</kbd>+<kbd>ENTER</kbd></b>.<br/><br/> Note: codebase indexing must finish before you can run this!</p>,
+      description: <p >Almost done! Try asking anything about your general codebase by prompting then pressing <b><kbd>{getMetaKeyAndShortcutLabel()}</kbd>+<kbd>ENTER</kbd></b>.<br /><br /> Note: codebase indexing must finish before you can run this!</p>,
       examples: [
         "What does my codebase do",
         "Where should I start to implement a feature about X"
@@ -169,7 +169,7 @@ const OnboardingTutorial: React.FC<OnboardingTutorialProps> = ({ onClose, onExam
     },
     {
       title: <h3>Toggle PearAI Inventory</h3>,
-      description: <p>Lastly, press <b><kbd>{getMetaKeyAndShortcutLabel()}</kbd>+<kbd>E</kbd></b> to toggle <b>PearAI Inventory</b>, and try out <strong>Creator</strong> and <strong>Search</strong> directly in there! <br/><br/>Enjoy PearAI! If you have questions, feel free to ask us in our <a href="https://discord.gg/7QMraJUsQt">Discord</a>, or through <a href="mailto:pear@trypear.ai">email</a>.</p>,
+      description: <p>Lastly, press <b><kbd>{getMetaKeyAndShortcutLabel()}</kbd>+<kbd>E</kbd></b> to toggle <b>PearAI Inventory</b>, and try out <strong>Creator</strong> and <strong>Search</strong> directly in there! <br /><br />Enjoy PearAI! If you have questions, feel free to ask us in our <a href="https://discord.gg/7QMraJUsQt">Discord</a>, or through <a href="mailto:pear@trypear.ai">email</a>.</p>,
     },
   ]
 
@@ -220,7 +220,7 @@ const OnboardingTutorial: React.FC<OnboardingTutorialProps> = ({ onClose, onExam
       if (currentPage === 2) {
         // Wait 100ms for quick input widget to appear
         await new Promise(resolve => setTimeout(resolve, 100));
-        ideMessenger.post("highlightElement", {elementSelectors: ['.quick-input-widget']});
+        ideMessenger.post("highlightElement", { elementSelectors: ['.quick-input-widget'] });
         nextPage();
       }
     },
@@ -243,41 +243,40 @@ const OnboardingTutorial: React.FC<OnboardingTutorialProps> = ({ onClose, onExam
           nextPage();
         }
         if (['Enter', 'ArrowLeft', 'ArrowRight', 'Escape'].includes(event.key)) {
-          ideMessenger.post("unhighlightElement", {elementSelectors: ['.quick-input-widget']});
+          ideMessenger.post("unhighlightElement", { elementSelectors: ['.quick-input-widget'] });
         }
       };
-  
+
       window.addEventListener('keydown', handleEnterKey);
-  
+
       // Cleanup function
       return () => {
-        ideMessenger.post("unhighlightElement", {elementSelectors: ['.quick-input-widget']});
+        ideMessenger.post("unhighlightElement", { elementSelectors: ['.quick-input-widget'] });
       };
     }
   }, [currentPage]); // Include all dependencies
 
   useEffect(() => {
-      window.addEventListener('keydown', handleKeyDown);
-      return () => window.removeEventListener('keydown', handleKeyDown);
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
   }, [currentPage]);
-  
+
   return (
     <TutorialCardBorder>
-    <TutorialCardDiv className={`flex flex-col p-2 justify-between bg-background text-sm overflow-hidden text-input-foreground`}>
-      <div className="mb-3">
-      <div
-          onClick={onClose}
-          className="absolute underline top-2 right-2 p-1 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full cursor-pointer shadow-sm"
-          role="button"
-          aria-label="Close"
-        >
-          Close
-      </div>
+      <TutorialCardDiv className={`flex flex-col p-2 justify-between bg-background text-sm overflow-hidden text-input-foreground`}>
+        <div className="mb-3">
+          <div
+            onClick={onClose}
+            className="absolute underline top-2 right-2 p-1 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full cursor-pointer shadow-sm"
+            role="button"
+            aria-label="Close"
+          >
+            Close
+          </div>
           <div className="flex flex-col justify-between mt-1">
             <div>
-              <div className="flex justify-between items-center ">
-              Learn how to use PearAI chat
-              </div>
+              <h2 className="text-xl font-semibold tracking-tight text-white">Learn how to use PearAI chat</h2>
+
               <ContentWrapper
                 direction={slideDirection}
                 key={currentPage}
@@ -299,43 +298,43 @@ const OnboardingTutorial: React.FC<OnboardingTutorialProps> = ({ onClose, onExam
                       <Lightbulb size={13} />
                       <span>Try these examples</span>
                     </ExamplesHeader>
-                      <div className="flex flex-wrap gap-1">
-                        {currentPageData.examples.map((example) => (
-                          <CopyButtonWithText
-                            key={example}
-                            text={example}
-                            side="top"
-                            variant="ghost"
-                            onTextClick={onExampleClick}
-                          />
-                        ))}
-                      </div>
+                    <div className="flex flex-wrap gap-1">
+                      {currentPageData.examples.map((example) => (
+                        <CopyButtonWithText
+                          key={example}
+                          text={example}
+                          side="top"
+                          variant="ghost"
+                          onTextClick={onExampleClick}
+                        />
+                      ))}
+                    </div>
                   </ExamplesSection>
                 )}
               </ContentWrapper>
             </div>
           </div>
-      </div>
-      <div className="pl-1 justify-end items-center gap-2 inline-flex">
-        <Button 
-          size="icon" 
-          onClick={prevPage} 
-          disabled={currentPage === 0}
-          className="h-6 w-6"
-        >
-          <ChevronLeft color="background"/>
-        </Button>
-        <span className="text-xs">{currentPage + 1} / {pages.length}</span>
-        <Button 
-          size="icon" 
-          onClick={nextPage} 
-          disabled={currentPage === pages.length - 1}
-          className="h-6 w-6"
-        >
-          <ChevronRight color="background"/>
-        </Button>
-      </div>
-    </TutorialCardDiv>
+        </div>
+        <div className="pl-1 justify-end items-center gap-2 inline-flex">
+          <Button
+            size="icon"
+            onClick={prevPage}
+            disabled={currentPage === 0}
+            className="h-6 w-6"
+          >
+            <ChevronLeft color="background" />
+          </Button>
+          <span className="text-xs">{currentPage + 1} / {pages.length}</span>
+          <Button
+            size="icon"
+            onClick={nextPage}
+            disabled={currentPage === pages.length - 1}
+            className="h-6 w-6"
+          >
+            <ChevronRight color="background" />
+          </Button>
+        </div>
+      </TutorialCardDiv>
     </TutorialCardBorder>
   );
 };
