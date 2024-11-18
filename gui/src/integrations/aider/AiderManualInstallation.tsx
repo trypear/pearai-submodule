a67700aa2883646bef7ee45e8d3b4516dcc60e73
Himanshu <him.speedo@gmail.com>
2024-11-18 21:26:17 +0530

    fixes

--- gui/src/integrations/aider/AiderManualInstallation.tsx
+++ gui/src/integrations/aider/AiderManualInstallation.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { getPlatform } from "../../util";
 
-
 const AiderManualInstallation: React.FC = () => {
   const platform = getPlatform();
 
@@ -12,33 +11,26 @@ const AiderManualInstallation: React.FC = () => {
 
   return (
     <div className="flex items-center justify-center h-screen overflow-y-auto">
-
-    <div className="p-6 bg-input rounded-lg shadow-md ">
-      <h2 className="text-2xl font-bold mb-4">Manual Installation Guide for PearAI Creator (Powered by aider)</h2>
-      <p className="mb-4">
-        Automatic installation of PearAI Creator (Powered by aider) was
-        unsuccessful. Please follow the steps below to manually install it to
-        get it working.
-      </p>
-      {instructions}
-
-      <p className="mt-4 bg-statusbar-background p-4 rounded-lg">
-        If you followed above instructions correctly and restarted PearAI, then PearAI
-        Creator should work! 
-        <br />
-        If not, please view{" "}
-        <a className="text-blue-500 hover:underline" href="https://trypear.ai/creator-troubleshooting">
-          PearAI Troubleshooting
-        </a>
-        , or contact PearAI Support on{" "}
-        <a className="text-blue-500 hover:underline" href="https://discord.gg/avc2y2Kqsa">Discord</a>.
-      </p>
+      <div className="p-6 bg-input rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Manual Installation Guide for PearAI Creator (Powered by aider)</h2>
+        <p className="mb-4">
+          Automatic installation of PearAI Creator (Powered by aider) was unsuccessful. Please follow the steps below to manually install it to get it working.
+        </p>
+        {instructions}
+        <p className="mt-4 bg-statusbar-background p-4 rounded-lg">
+          If you followed the above instructions correctly and restarted PearAI, then PearAI Creator should work! 
+          <br />
+          If not, please view{" "}
+          <a className="text-blue-500 hover:underline" href="https://trypear.ai/creator-troubleshooting">
+            PearAI Troubleshooting
+          </a>
+          , or contact PearAI Support on{" "}
+          <a className="text-blue-500 hover:underline" href="https://discord.gg/avc2y2Kqsa">Discord</a>.
+        </p>
+      </div>
     </div>
-    </div>
-
   );
 };
-
 
 const WindowsManualInstallation: React.FC = () => {
   const pythonCmd = "winget install Python.Python.3.9";
@@ -54,7 +46,7 @@ const WindowsManualInstallation: React.FC = () => {
         <li className="mb-2">
           <strong>Install Python - </strong> If not already installed, run:
           <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center flex-wrap">
               <span className="font-mono">{pythonCmd}</span>
               <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
                 onClick={() => navigator.clipboard.writeText(pythonCmd)}
@@ -65,7 +57,7 @@ const WindowsManualInstallation: React.FC = () => {
         <li className="mb-2">
           <strong>Install aider - </strong> Please run:
           <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center flex-wrap">
               <span className="font-mono">{aiderCmd}</span>
               <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
                 onClick={() => navigator.clipboard.writeText(aiderCmd)}
@@ -78,60 +70,60 @@ const WindowsManualInstallation: React.FC = () => {
         </li>
       </ol>
     </div>
-  )
+  );
 }
 
 const MacManualInstallation: React.FC = () => {
-  const homebrewCmd = "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"";
+  const homebrewCmd = '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"';
   const pythonCmd = "brew install python@3";
   const aiderCmd = "python -m pip install -U aider-chat";
 
   return (
-      <div className="p-4 bg-statusbar-background rounded-lg shadow-md">
-        <h3 className="text-xl font-semibold mb-2">For macOS/Linux:</h3>
-        <ol className="list-decimal list-inside">
-          <li className="mb-4">
-            <strong>Open a new terminal window</strong>
-          </li>
-          <li className="mb-2">
-            <strong>Install Homebrew - </strong> If not already installed, run:
-            <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
-              <div className="flex justify-between items-center">
-                <span className="font-mono">{homebrewCmd}</span>
-                <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
-                  onClick={() => navigator.clipboard.writeText(homebrewCmd)}
-                >copy</span>
-              </div>
-            </pre>
-          </li>
-          <li className="mb-2">
-            <strong>Install Python - </strong> Please run:
-            <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
-              <div className="flex justify-between items-center">
-                <span className="font-mono">{pythonCmd}</span>
-                <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
-                  onClick={() => navigator.clipboard.writeText(pythonCmd)}
-                >copy</span>
-              </div>
-            </pre>
-          </li>
-          <li className="mb-2">
-            <strong>Install aider - </strong> Please run:
-            <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
-              <div className="flex justify-between items-center">
-                <span className="font-mono">{aiderCmd}</span>
-                <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
-                  onClick={() => navigator.clipboard.writeText(aiderCmd)}
-                >copy</span>
-              </div>
-            </pre>
-          </li>
-          <li className="mt-4">
-            <strong>Finally, please close and reopen PearAI</strong>
-          </li>
-        </ol>
-      </div>
-  )
+    <div className="p-4 bg-statusbar-background rounded-lg shadow-md flex-wrap text-wrap">
+      <h3 className="text-xl font-semibold mb-2">For macOS/Linux:</h3>
+      <ol className="list-decimal list-inside">
+        <li className="mb-4">
+          <strong>Open a new terminal window</strong>
+        </li>
+        <li className="mb-2">
+          <strong>Install Homebrew - </strong> If not already installed, run:
+          <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
+            <div className="flex justify-between items-center flex-wrap">
+              <span className="font-mono text-wrap">{homebrewCmd}</span>
+              <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
+                onClick={() => navigator.clipboard.writeText(homebrewCmd)}
+              >copy</span>
+            </div>
+          </pre>
+        </li>
+        <li className="mb-2">
+          <strong>Install Python - </strong> Please run:
+          <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
+            <div className="flex justify-between items-center flex-wrap">
+              <span className="font-mono text-wrap">{pythonCmd}</span>
+              <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
+                onClick={() => navigator.clipboard.writeText(pythonCmd)}
+              >copy</span>
+            </div>
+          </pre>
+        </li>
+        <li className="mb-2">
+          <strong>Install aider - </strong> Please run:
+          <pre className="bg-secondary border-solid border-2 border-input p-2 rounded-lg">
+            <div className="flex justify-between items-center flex-wrap">
+              <span className="font-mono text-wrap">{aiderCmd}</span>
+              <span className="font-mono ml-auto bg-button-background text-button-foreground border-solid border-2 border-input cursor-pointer px-2 py-1 rounded-md"
+                onClick={() => navigator.clipboard.writeText(aiderCmd)}
+              >copy</span>
+            </div>
+          </pre>
+        </li>
+        <li className="mt-4">
+          <strong>Finally, please close and reopen PearAI</strong>
+        </li>
+      </ol>
+    </div>
+  );
 }
 
 export default AiderManualInstallation;
