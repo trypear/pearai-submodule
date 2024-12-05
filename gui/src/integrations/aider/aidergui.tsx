075472290c4cf8a647fccf7ffa3968f8f4afa365
John Pan <john.pan@mail.mcgill.ca>
2024-12-05 12:30:25 -0800

    focus editor on new session and history load (#211)
    * focus editor on new session and history load
    
    * remove comments

--- gui/src/integrations/aider/aidergui.tsx
+++ gui/src/integrations/aider/aidergui.tsx
@@ -52,6 +52,7 @@ function AiderGUI() {
   const navigate = useNavigate();
   const ideMessenger = useContext(IdeMessengerContext);
 
+  const [sessionKey, setSessionKey] = useState(0);
   const sessionState = useSelector((state: RootState) => state.state);
   const defaultModel = useSelector(defaultModelSelector);
   const active = useSelector((state: RootState) => state.state.aiderActive);
@@ -195,7 +196,7 @@ function AiderGUI() {
     "newSession",
     async () => {
       saveSession();
-      mainTextInputRef.current?.focus?.();
+      setSessionKey(prev => prev + 1);
     },
     [saveSession],
   );
@@ -394,6 +395,7 @@ useEffect(() => {
                       onClick={() => {
                         saveSession();
                         ideMessenger.post("aiderResetSession", undefined);
+                        setSessionKey(prev => prev + 1);
                       }}
                       className="mr-auto"
                     >
@@ -518,6 +520,7 @@ useEffect(() => {
               )}
             >
               <ContinueInputBox
+                key={sessionKey}
                 onEnter={(editorContent, modifiers) => {
                   sendInput(editorContent, modifiers);
                 }}
@@ -544,6 +547,7 @@ useEffect(() => {
                 onClick={() => {
                   saveSession();
                   ideMessenger.post("aiderResetSession", undefined);
+                  setSessionKey(prev => prev + 1);
                 }}
                 className="mr-auto"
               >
