3796a4f84edb250c46d7506d667d72533bd8d018
Himanshu <him.speedo@gmail.com>
2024-12-22 12:33:17 +0530

    hide icons on small screen

--- gui/src/integrations/pearaiWrapped/pearaiWrappedGui.tsx
+++ gui/src/integrations/pearaiWrapped/pearaiWrappedGui.tsx
@@ -39,146 +39,147 @@ export default function PearAIWrappedGUI() {
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-primary">
       <div className="absolute inset-0 pointer-events-none z-0">
-        <div className="absolute top-[13%] left-[12%] opacity-5">
-          <TreePine className="w-16 h-16" />
+        <div className="hidden lg:block">
+          <div className="absolute top-[13%] left-[12%] opacity-5">
+            <TreePine className="w-16 h-16" />
+          </div>
+          <div className="absolute top-[12%] right-[18%] opacity-5">
+            <Github className="w-14 h-14" />
+          </div>
+          
+          {/* <motion.div
+            className="absolute top-[22%] right-[72%] opacity-5"
+            animate={{ y: [0, -20, 0] }}
+            transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Star className="w-12 h-12" />
+          </motion.div> */}
+          <motion.div
+            className="absolute top-[36%] left-[4%] opacity-5"
+            animate={{ y: [0, -20, 0] }}
+            transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Star className="w-14 h-14" />
+          </motion.div>
+          <motion.div
+            className="absolute top-[16%] left-[58%] opacity-5"
+            animate={{ y: [0, -15, 0] }}
+            transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Timer className="w-20 h-20" />
+          </motion.div>
+          
+          <motion.div
+            className="absolute top-[45%] left-[15%] opacity-5"
+            animate={{ y: [0, 15, 0] }}
+            transition={{ duration: 7, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Code2 className="w-24 h-24" />
+          </motion.div>
+          <div className="absolute top-[37%] right-[6%] opacity-5">
+            <Hash className="w-16 h-16" />
+          </div>
+          
+          <motion.div
+            className="absolute top-[20%] left-[38%] opacity-5"
+            animate={{ y: [0, 12, 0] }}
+            transition={{ duration: 5.2, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Github className="w-12 h-12" />
+          </motion.div>
+          <motion.div
+            className="absolute top-[30%] right-[20%] opacity-5"
+            animate={{ y: [0, -14, 0] }}
+            transition={{ duration: 4.8, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Code2 className="w-16 h-16" />
+          </motion.div>
+          
+          <motion.div
+            className="absolute bottom-[12%] left-[8%] opacity-5"
+            animate={{ y: [0, 20, 0] }}
+            transition={{ duration: 5, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Sparkles className="w-28 h-28" />
+          </motion.div>
+          {/* <motion.div
+            className="absolute bottom-[38%] right-[10%] opacity-5"
+            animate={{ y: [0, -18, 0] }}
+            transition={{ duration: 5.5, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Star className="w-26 h-26" />
+          </motion.div> */}
+          
+          <motion.div
+            className="absolute bottom-[9%] left-[62%] opacity-5"
+            animate={{ y: [0, 16, 0] }}
+            transition={{ duration: 6.2, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Hash className="w-20 h-20" />
+          </motion.div>
+          <motion.div
+            className="absolute bottom-[33%] right-[64%] opacity-5"
+            animate={{ y: [0, -15, 0] }}
+            transition={{ duration: 5.8, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <GitBranch className="w-24 h-24" />
+          </motion.div>
+          
+          <div className="absolute bottom-[6%] left-[32%] opacity-5">
+            <GitBranch className="w-24 h-24" />
+          </div>
+          <motion.div
+            className="absolute bottom-[27%] right-[22%] opacity-5"
+            animate={{ y: [0, 20, 0] }}
+            transition={{ duration: 6.2, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <TreePine className="w-20 h-20" />
+          </motion.div>
+          <motion.div
+            className="absolute bottom-[13%] right-[10%] opacity-5"
+            animate={{ y: [0, 20, 0] }}
+            transition={{ duration: 6.2, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Gift className="w-24 h-24" />
+          </motion.div>
+          <motion.div
+            className="absolute bottom-[6%] right-[44%] opacity-5"
+            animate={{ y: [0, 25, 0] }}
+            transition={{ duration: 6.5, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Code className="w-14 h-14" />
+          </motion.div>
+          
+          <motion.div
+            className="absolute top-[8%] right-[28%] opacity-5"
+            animate={{ y: [0, -15, 0] }}
+            transition={{ duration: 5.3, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Snowflake className="w-16 h-16" />
+          </motion.div>
+          
+          <motion.div
+            className="absolute bottom-[65%] left-[20%] opacity-5"
+            animate={{ y: [0, 18, 0] }}
+            transition={{ duration: 4.8, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Candy className="w-20 h-20" />
+          </motion.div>
+          <motion.div
+            className="absolute bottom-[18%] left-[45%] opacity-5"
+            animate={{ y: [0, 15, 0] }}
+            transition={{ duration: 5.7, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Snowflake className="w-24 h-24" />
+          </motion.div>
+          <motion.div
+            className="absolute bottom-[34%] right-[10%] opacity-5"
+            animate={{ y: [0, 15, 0] }}
+            transition={{ duration: 5.7, repeat: Infinity, ease: "easeInOut" }}
+          >
+            <Snowflake className="w-16 h-16" />
+          </motion.div>
         </div>
-        <div className="absolute top-[12%] right-[18%] opacity-5">
-          <Github className="w-14 h-14" />
-        </div>
-        
-        {/* <motion.div
-          className="absolute top-[22%] right-[72%] opacity-5"
-          animate={{ y: [0, -20, 0] }}
-          transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Star className="w-12 h-12" />
-        </motion.div> */}
-        <motion.div
-          className="absolute top-[36%] left-[4%] opacity-5"
-          animate={{ y: [0, -20, 0] }}
-          transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Star className="w-14 h-14" />
-        </motion.div>
-        <motion.div
-          className="absolute top-[16%] left-[58%] opacity-5"
-          animate={{ y: [0, -15, 0] }}
-          transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Timer className="w-20 h-20" />
-        </motion.div>
-        
-        <motion.div
-          className="absolute top-[45%] left-[15%] opacity-5"
-          animate={{ y: [0, 15, 0] }}
-          transition={{ duration: 7, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Code2 className="w-24 h-24" />
-        </motion.div>
-        <div className="absolute top-[37%] right-[6%] opacity-5">
-          <Hash className="w-16 h-16" />
-        </div>
-        
-        <motion.div
-          className="absolute top-[20%] left-[38%] opacity-5"
-          animate={{ y: [0, 12, 0] }}
-          transition={{ duration: 5.2, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Github className="w-12 h-12" />
-        </motion.div>
-        <motion.div
-          className="absolute top-[30%] right-[20%] opacity-5"
-          animate={{ y: [0, -14, 0] }}
-          transition={{ duration: 4.8, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Code2 className="w-16 h-16" />
-        </motion.div>
-        
-        <motion.div
-          className="absolute bottom-[12%] left-[8%] opacity-5"
-          animate={{ y: [0, 20, 0] }}
-          transition={{ duration: 5, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Sparkles className="w-28 h-28" />
-        </motion.div>
-        {/* <motion.div
-          className="absolute bottom-[38%] right-[10%] opacity-5"
-          animate={{ y: [0, -18, 0] }}
-          transition={{ duration: 5.5, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Star className="w-26 h-26" />
-        </motion.div> */}
-        
-        <motion.div
-          className="absolute bottom-[9%] left-[62%] opacity-5"
-          animate={{ y: [0, 16, 0] }}
-          transition={{ duration: 6.2, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Hash className="w-20 h-20" />
-        </motion.div>
-        <motion.div
-          className="absolute bottom-[33%] right-[64%] opacity-5"
-          animate={{ y: [0, -15, 0] }}
-          transition={{ duration: 5.8, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <GitBranch className="w-24 h-24" />
-        </motion.div>
-        
-        <div className="absolute bottom-[6%] left-[32%] opacity-5">
-          <GitBranch className="w-24 h-24" />
-        </div>
-        <motion.div
-          className="absolute bottom-[27%] right-[22%] opacity-5"
-          animate={{ y: [0, 20, 0] }}
-          transition={{ duration: 6.2, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <TreePine className="w-20 h-20" />
-        </motion.div>
-        <motion.div
-          className="absolute bottom-[13%] right-[10%] opacity-5"
-          animate={{ y: [0, 20, 0] }}
-          transition={{ duration: 6.2, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Gift className="w-24 h-24" />
-        </motion.div>
-        <motion.div
-          className="absolute bottom-[6%] right-[44%] opacity-5"
-          animate={{ y: [0, 25, 0] }}
-          transition={{ duration: 6.5, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Code className="w-14 h-14" />
-        </motion.div>
-        
-        <motion.div
-          className="absolute top-[8%] right-[28%] opacity-5"
-          animate={{ y: [0, -15, 0] }}
-          transition={{ duration: 5.3, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Snowflake className="w-16 h-16" />
-        </motion.div>
-        
-        <motion.div
-          className="absolute bottom-[65%] left-[20%] opacity-5"
-          animate={{ y: [0, 18, 0] }}
-          transition={{ duration: 4.8, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Candy className="w-20 h-20" />
-        </motion.div>
-        <motion.div
-          className="absolute bottom-[18%] left-[45%] opacity-5"
-          animate={{ y: [0, 15, 0] }}
-          transition={{ duration: 5.7, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Snowflake className="w-24 h-24" />
-        </motion.div>
-        <motion.div
-          className="absolute bottom-[34%] right-[10%] opacity-5"
-          // className="absolute bottom-[18%] left-[45%] opacity-5"
-          animate={{ y: [0, 15, 0] }}
-          transition={{ duration: 5.7, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <Snowflake className="w-16 h-16" />
-        </motion.div>
       </div>
 
       <div className="flex flex-col items-center gap-6 z-10">
