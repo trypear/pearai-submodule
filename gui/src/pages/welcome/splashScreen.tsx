cb9fbaa57ca8ade9bbba8462a069fa1df8ca3128
Himanshu <him.speedo@gmail.com>
2025-02-27 11:39:35 +0530

    pear logo

--- gui/src/pages/welcome/splashScreen.tsx
+++ gui/src/pages/welcome/splashScreen.tsx
@@ -7,9 +7,9 @@ const SplashScreen = ({ onNext }: { onNext: () => void }) => {
     return (
         <div className="h-full flex-col justify-center items-center gap-10 inline-flex overflow-hidden select-none">
             <div className="max-w-2xl mx-auto text-center flex flex-col gap-7 justify-center">
-                <InventoryButtons />
-                <div className="flex-col justify-start items-start gap-7 flex">
-                    <img src={getLogoPath("pearai-chat-splash.svg")} alt="..." />
+                {/* <InventoryButtons /> */}
+                <div className="flex-col justify-center items-center gap-7 flex w-64 mx-auto">
+                    <img src={getLogoPath("pearai-color.png")} alt="..." />
                 </div>
                 <div className="text-4xl font-['SF Pro']">Welcome to PearAI</div>
                 <Button className="mx-auto w-[300px] rounded-lg justify-center items-center gap-1 inline-flex overflow-hidden" onClick={onNext}>
