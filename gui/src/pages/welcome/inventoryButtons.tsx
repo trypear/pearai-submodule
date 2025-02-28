4a458918aded57ba155433f95a87e1cc2217aa00
nang-dev <nathanang2000@gmail.com>
2025-02-28 01:12:59 -0500

    Reverted breaking welcome

--- gui/src/pages/welcome/inventoryButtons.tsx
+++ gui/src/pages/welcome/inventoryButtons.tsx
@@ -13,6 +13,7 @@ const menuItems = [
         backgroundColor: "bg-gradient-to-b from-[#ff70bc] to-[#cc237e]",
         textColor: "#ffcee3",
         glow: "shadow-[0px_0px_57.60000228881836px_9.600000381469727px_rgba(255,113,189,1.00)]",
+        expandedWidth: "w-[83px]",
         boxShadow: "shadow-[inset_0px_0px_0px_1.2px_#FFFFFF80]",
         textMargin: "mb-[2.7px]"
     },
@@ -24,6 +25,7 @@ const menuItems = [
         bgGradient: "bg-gradient-to-b from-[#aff349] to-[#1b9300]",
         textColor: "#e8ffc7",
         glow: "shadow-[0px_0px_57.60000228881836px_9.600000381469727px_rgba(175,243,73,1.00)]",
+        expandedWidth: "w-[65px]",
         boxShadow: "shadow-[inset_0px_0px_0px_1.2px_#FFFFFF80]",
         textMargin: "mb-[2.5px]"
     },
@@ -35,6 +37,7 @@ const menuItems = [
         backgroundColor: "bg-gradient-to-b from-[#12d3cd] to-[#008872]",
         textColor: "#c1fff2",
         glow: "shadow-[0px_0px_57.60000228881836px_9.600000381469727px_rgba(20,210,203,1.00)] ",
+        expandedWidth: "w-[78px]",
         boxShadow: "shadow-[inset_0px_0px_0px_1.2px_#FFFFFF80]",
         textMargin: "mb-[2.5px]"
     },
@@ -46,6 +49,7 @@ const menuItems = [
         backgroundColor: "bg-gradient-to-b from-[#9069fe] to-[#582bd3]",
         textColor: "#e0d5ff",
         glow: "shadow-[0px_0px_57.60000228881836px_9.600000381469727px_rgba(142,102,252,1.00)]",
+        expandedWidth: "w-[88px]",
         boxShadow: "shadow-[inset_0px_0px_0px_1.2px_#FFFFFF80]",
         textMargin: "mb-[2.6px]"
     }
@@ -54,7 +58,7 @@ const menuItems = [
 const InventoryButtons = ({ activeItemID = "agent" }: { activeItemID?: string }) => {
 
     // // this is for devving.
-    // const [activeItem, setActiveItem] = useState(activeItemID);
+    // const [activeItem, setActiveItem] = useState(activeItemID); 
 
     // const handleItemClick = (itemId: string) => {
     //     if (process.env.NODE_ENV !== "development") {
@@ -66,7 +70,7 @@ const InventoryButtons = ({ activeItemID = "agent" }: { activeItemID?: string })
     return (
         <div className={`z-10 select-none`}>
             <div className="flex cursor-pointer">
-                <div
+                <div 
                     className="overflow-hidden rounded-xl relative shadow-[inset_0px_0px_0px_1px_rgba(255,255,255,0.25)]"
                     style={{ background: vscSidebarBorder }}
                 >
@@ -75,18 +79,18 @@ const InventoryButtons = ({ activeItemID = "agent" }: { activeItemID?: string })
                             <div
                                 key={`${item.tooltip}-${index}`}
                                 className={` h-7
-                                    ${item.backgroundColor || item.bgGradient}
-                                    ${activeItemID === item.id ? item.glow : ""}
-                                    ${activeItemID !== item.id ? "z-10" : "z-5"}
+                                    ${item.backgroundColor || item.bgGradient} 
+                                    ${activeItemID === item.id ? item.glow : ""} 
+                                    ${activeItemID !== item.id ? "z-10" : "z-5"} 
                                     rounded-[10px] flex items-center
                                     transition-all duration-500 ease-in-out
-                                    ${activeItemID === item.id ? "w-auto" : "w-7"}
+                                    ${activeItemID === item.id ? item.expandedWidth : "w-7"}
                                     ${activeItemID === item.id ? item.boxShadow : ""}`}
                                 // onClick={() => handleItemClick(item.id)} // this is for devving.
                             >
-                                <div
-                                    className={`flex-shrink-0 w-7 flex items-center justify-center
-                                        ${activeItemID === item.id ? "ml-[2px]" : ""}
+                                <div 
+                                    className={`flex-shrink-0 w-7 flex items-center justify-center 
+                                        ${activeItemID === item.id ? "ml-[1px]" : ""}
                                         `}
                                 >
                                     <img
@@ -95,8 +99,8 @@ const InventoryButtons = ({ activeItemID = "agent" }: { activeItemID?: string })
                                         alt={item.tooltip}
                                     />
                                 </div>
-                                <span
-                                    className={`${item.textMargin} flex items-center text-center justify-center text-sm whitespace-nowrap overflow-hidden transition-all duration-200 pr-[10px]
+                                <span 
+                                    className={`${item.textMargin} flex items-center text-center justify-center text-sm whitespace-nowrap overflow-hidden transition-all duration-200
                                         ${activeItemID === item.id ? "opacity-100" : "opacity-0"}`}
                                     style={{ color: item.textColor }}
                                 >
