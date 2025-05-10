37308342e8935c47cedea661d5546ae6cf53b635
James Arnott <james@rno.tt>
2025-05-10 17:34:03 +0100

    chore: prettier

--- gui/src/pages/creator/rgbBackground.tsx
+++ gui/src/pages/creator/rgbBackground.tsx
@@ -1,10 +1,13 @@
-import { cn } from "@/lib/utils"
-import React, { FC, ReactNode } from "react"
+import { cn } from "@/lib/utils";
+import React, { FC, ReactNode } from "react";
 
 /**
  * RGB gradient border wrapper
  */
-export const RGBWrapper: FC<{ children: ReactNode; className?: string }> = ({ children, className }) => {
+export const RGBWrapper: FC<{ children: ReactNode; className?: string }> = ({
+  children,
+  className,
+}) => {
   return (
     <div className={cn(className, "relative")}>
       {/* RGB Gradient Border */}
@@ -12,12 +15,13 @@ export const RGBWrapper: FC<{ children: ReactNode; className?: string }> = ({ ch
         className="absolute inset-0 -z-10 rounded-xl opacity-75 blur-[30px]"
         style={{
           border: "40px solid transparent",
-          borderImage: "linear-gradient(to bottom right, #5DED83 0%, #0CBBAF 33%, #764CEA 66%, #EA50A2 100%) 1"
+          borderImage:
+            "linear-gradient(to bottom right, #5DED83 0%, #0CBBAF 33%, #764CEA 66%, #EA50A2 100%) 1",
         }}
       ></div>
 
       {/* Content */}
       {children}
     </div>
-  )
-}
+  );
+};
